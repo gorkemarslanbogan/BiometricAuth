dfeed61964418d362bf018d7e5cd308b38924eab
gorkemarslanbogan <gorkemarslanbogan@gmail.com>
2023-09-27 13:23:37 +0300

    hello again

---
+++ lib/helper/ui_helper.dart
@@ -0,0 +1,30 @@
+
+
+class UiHelper {
+  //Strings
+  static const LoginApp = 'Login App'; 
+  static const WelcomeText = 'Welcome';
+  static const LoginScreen = 'Login Screen';
+  static const LoginScreenText = 'Hey \n Login Now.';
+  static const CreateAccount = 'If you are new / Create account';
+  static const Email = 'Email';
+  static const Password = 'Password';
+  static const ForgetPassword = 'Forget Password?';
+  static const Login = 'Login';
+  static const Or = 'Or';
+  static const TouchId = 'Touch ID';
+  static const CreateNewAccount = 'Create \n new account';
+  static const Skip = 'Skip';
+  static const FirstName = 'FirstName';
+  static const LastName = 'LastName';
+  static const BirthDay = 'Birth Day';
+  static const Confirm = 'Confirm';
+  
+
+
+
+
+
+
+
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,18 @@
+import 'package:animal_project/helper/ui_helper.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text(UiHelper.WelcomeText),),
+    );
+  }
+}
